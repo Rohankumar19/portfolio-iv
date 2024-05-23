496ff0f34b17ada8a7cd0f681285c6e2cb70e3b1
Rohan Kumar <rohankumar19003@gmail.com>
2024-05-23 18:48:06 +0530

    Add files via upload

---
+++ src/configs/launchpad.js
@@ -0,0 +1,64 @@
+const launchpadApps = [
+  {
+    id: "flint",
+    title: "Flint",
+    img: "img/icons/launchpad/flint.png",
+    link: "https://github.com/umair-akb/"
+  },
+  {
+    id: "metallic",
+    title: "Metallic",
+    img: "img/icons/launchpad/meta.png",
+    link: "https://github.com/umair-akb/"
+  },
+  {
+    id: "alkaid",
+    title: "Alkaid",
+    img: "img/icons/launchpad/rl.png",
+    link: "https://github.com/umair-akb/"
+  },
+  {
+    id: "oh-vue-icons",
+    title: "Oh, Vue Icons!",
+    img: "img/icons/launchpad/icon.png",
+    link: "https://github.com/umair-akb/"
+  },
+  {
+    id: "gungnir",
+    title: "Gungnir",
+    img: "img/icons/launchpad/gungnir.png",
+    link: "https://github.com/umair-akb/"
+  },
+  {
+    id: "what-if",
+    title: "My Notebook",
+    img: "img/icons/launchpad/notebook.png",
+    link: "https://github.com/umair-akb/"
+  },
+  {
+    id: "cube",
+    title: "Cube Solver",
+    img: "img/icons/launchpad/cube.png",
+    link: "https://github.com/umair-akb/"
+  },
+  {
+    id: "resume",
+    title: "Résumé",
+    img: "img/icons/launchpad/resume.png",
+    link: "https://github.com/umair-akb/"
+  },
+  {
+    id: "zelda",
+    title: "Webpage for Zelda",
+    img: "img/icons/launchpad/zelda.png",
+    link: "https://github.com/umair-akb/"
+  },
+  {
+    id: "fishmail",
+    title: "Fishmail",
+    img: "img/icons/launchpad/fishmail.png",
+    link: "https://github.com/umair-akb/"
+  }
+];
+
+export default launchpadApps;
